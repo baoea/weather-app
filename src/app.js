c3a7f4ad72b818e8ce347e80b62c4f78305074f3
baotvdn <baotvdn@gmail.com>
2021-10-26 14:16:12 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -106,6 +107,6 @@ app.get('*', (req, res) => {
 })
 
 // Listener
-app.listen(3000, () => {
-    console.log('Port 3000 is up!')
+app.listen(port, () => {
+    console.log('Port ' + port + ' is up!')
 })
